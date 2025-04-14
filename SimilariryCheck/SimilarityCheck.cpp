1f34cf9e26741017ac87d16c96179b17b0cc0af2
Jonghwi <ellia2431@naver.com>
2025-04-14 09:14:49 +0900

    [feature] add function to check string length

--- SimilariryCheck/SimilarityCheck.cpp
+++ SimilariryCheck/SimilarityCheck.cpp
@@ -1,1 +1,11 @@
-#include "gmock/gmock.h"
+#include <string>
+
+using std::string;
+
+class SimilarityChecker {
+public:
+	int getCharCountScore(string str1, string str2) {
+		if (str1.length() == str2.length())
+			return 60;
+	}
+};
--- SimilariryCheck/SimilarityCheck_test.cpp
+++ SimilariryCheck/SimilarityCheck_test.cpp
@@ -1,6 +1,13 @@
 #include "gmock/gmock.h"
 #include "SimilarityCheck.cpp"
 
-TEST(SimilarityCheck, tc1) {
-	EXPECT_EQ(1, 1);
+TEST(SimilarityCheck, checkCharCount) {
+	SimilarityChecker checker;
+	string str1 = "AAA";
+	string str2 = "BBB";
+
+	int expected = 60;
+	int actual = checker.getCharCountScore(str1, str2);
+
+	EXPECT_EQ(expected, actual);
 }

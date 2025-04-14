550244fe4a32dcb886181c534f30f4cb9db6abe3
Jonghwi <ellia2431@naver.com>
2025-04-14 09:19:54 +0900

    [refactoring] function to check string length

--- SimilariryCheck/SimilarityCheck.cpp
+++ SimilariryCheck/SimilarityCheck.cpp
@@ -4,8 +4,11 @@ using std::string;
 
 class SimilarityChecker {
 public:
-	int getCharCountScore(string str1, string str2) {
-		if (str1.length() == str2.length())
-			return 60;
+	int getLengthScore(string str1, string str2) {
+		int len1 = (int)str1.length();
+		int len2 = (int)str2.length();
+		if (len1 > len2) std::swap(len1, len2);
+
+		return (1 - ((double)(len2 - len1) / len2)) * 60;
 	}
 };
--- SimilariryCheck/SimilarityCheck_test.cpp
+++ SimilariryCheck/SimilarityCheck_test.cpp
@@ -1,13 +1,24 @@
 #include "gmock/gmock.h"
 #include "SimilarityCheck.cpp"
 
-TEST(SimilarityCheck, checkCharCount) {
+TEST(SimilarityCheck, checkCharCount1) {
 	SimilarityChecker checker;
 	string str1 = "AAA";
 	string str2 = "BBB";
 
 	int expected = 60;
-	int actual = checker.getCharCountScore(str1, str2);
+	int actual = checker.getLengthScore(str1, str2);
+
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(SimilarityCheck, checkCharCount2) {
+	SimilarityChecker checker;
+	string str1 = "AAA";
+	string str2 = "BB";
+
+	int expected = 40;
+	int actual = checker.getLengthScore(str1, str2);
 
 	EXPECT_EQ(expected, actual);
 }
